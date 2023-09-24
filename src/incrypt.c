54080f80f7804e5614911d22955d1dfb8ae3763a
spetyak <smpetyak@gmail.com>
2023-09-24 13:58:53 -0400

    Added Outline

--- src/incrypt.c
+++ src/incrypt.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
+int main(int argc, char** argv)
 {
+
+    // add algorithm
+
+    // read in plaintext 
+    // L read in 16 bytes at a time
+
+    // read in s-box
+
+    // generate round keys given key
+    // L 14 rounds for a 256-bit key
+
+    // perform round operations:
+    // L sub-bytes
+    // L shift rows
+    // L mix columns
+    // L add round key
 
     printf("Hello World!\n");
 
